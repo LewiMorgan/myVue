eaf8be6c54e3574d8d3eb01d6a1edbd07239cd9d
LewiMorgan <mi1476834200@gmail.com>
2023-09-23 15:07:46 +0800

    修改src
    vue项目的src修改为01_src_分析脚手架，方便网课做笔记

--- vue_test/01_src_分析脚手架/App.vue
+++ vue_test/01_src_分析脚手架/App.vue
@@ -1,21 +1,21 @@
 <template>
 	<div>
 		<img src="./assets/logo.png" alt="logo">
-		<School></School>
-		<Student></Student>
+    <SchoolName></SchoolName>
+    <StudentName></StudentName>
 	</div>
 </template>
 
 <script>
 	//引入组件
-	import School from './components/School'
-	import Student from './components/Student'
+	import SchoolName from './components/SchoolName'
+	import StudentName from './components/StudentName'
 
 	export default {
 		name:'App',
 		components:{
-			School,
-			Student
+			SchoolName,
+      StudentName
 		}
 	}
 </script>
