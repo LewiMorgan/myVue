552be91ed57c9375768a7f7f56222b0ea95d1636
LewiMorgan <mi1476834200@gmail.com>
2023-09-25 08:54:50 +0800

    65
    ref属性

---
+++ vue_test/02_src_ref属性/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <School ref="sch"></School>
+    <h2 v-text="msg" ref="title"></h2>
+    <button @click="showDom" ref="btn">点我输出dom</button>
+  </div>
+</template>
+
+<script>
+//引入组件
+import School from './components/School'
+export default {
+    name:'App',
+    components:{
+      School,
+    },
+    data(){
+      return {
+        msg:'Welcome'
+      }
+    },
+    methods:{
+      showDom(){
+        console.log(this.$refs.title)//真实dom元素
+        console.log(this.$refs.sch)//vc School组件De实例对象
+      }
+    }
+
+}
+</script>
---
+++ vue_test/02_src_ref属性/components/School.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="demo">
+        <h2>{{name}}</h2>
+        <h2>{{address}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'School',
+    data() {
+        return {
+            name:'atguigu',
+            address:'beijing'
+        }
+    },
+}
+</script>
+
+<style>
+    .demo{
+        background: gray;
+    }
+</style>
